0acc309dfe187ea792be22bd12beec4e0e7dd5d6
Steve McKenzie <steve@stevemckenzie.ca>
2013-07-30 15:33:53 -0700

    Typecast to a UIProgressView vs id when calling setProgress.

--- MBProgressHUD.m
+++ MBProgressHUD.m
@@ -674,7 +674,7 @@ static const CGFloat kDetailsLabelFontSize = 12.f;
 		detailsLabel.font = self.detailsLabelFont;
 	} else if ([keyPath isEqualToString:@"progress"]) {
 		if ([indicator respondsToSelector:@selector(setProgress:)]) {
-			[(id)indicator setProgress:progress];
+			[(UIProgressView *)indicator setProgress:progress];
 		}
 		return;
 	}
